58bd9328c166b2059f0b4c5eeb4e8e48796de699
Kyle Chisholm <dev@kylechisholm.ca>
2020-04-04 21:17:38 -0400

    Task MP.2 complete: keypoint detection

--- src/MidTermProject_Camera_Student.cpp
+++ src/MidTermProject_Camera_Student.cpp
@@ -75,24 +75,28 @@ int main(int argc, const char *argv[])
 
         // extract 2D keypoints from current image
         vector<cv::KeyPoint> keypoints; // create empty feature list for current image
-        string detectorType = "SHITOMASI";
+        string detectorType = "SIFT";
 
-        //! STUDENT ASSIGNMENT
-        //! TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
-        //! -> HARRIS, FAST, BRISK, ORB, AKAZE, SIFT
+        //// STUDENT ASSIGNMENT
+        //// TASK MP.2 -> add the following keypoint detectors in file matching2D.cpp and enable string-based selection based on detectorType
+        //// -> HARRIS, FAST, BRISK, ORB, AKAZE, SIFT
 
         if (detectorType.compare("SHITOMASI") == 0)
         {
             detKeypointsShiTomasi(keypoints, imgGray, false);
         }
+        else if (detectorType.compare("HARRIS") == 0)
+        {
+            detKeypointsHarris(keypoints, imgGray, false);
+        }
         else
         {
-            //...
+            detKeypointsModern(keypoints, imgGray, detectorType, false);
         }
-        //! EOF STUDENT ASSIGNMENT
+        //// EOF STUDENT ASSIGNMENT
 
-        //! STUDENT ASSIGNMENT
-        //! TASK MP.3 -> only keep keypoints on the preceding vehicle
+        // STUDENT ASSIGNMENT
+        // TASK MP.3 -> only keep keypoints on the preceding vehicle
 
         // only keep keypoints on the preceding vehicle
         bool bFocusOnVehicle = true;
--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -101,3 +101,122 @@ void detKeypointsShiTomasi(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool b
         cv::waitKey(0);
     }
 }
+
+void detKeypointsHarris(vector<cv::KeyPoint> &keypoints, cv::Mat &img, bool bVis)
+{
+    // Detector parameters
+    int blockSize = 2;     // for every pixel, a blockSize × blockSize neighborhood is considered
+    int apertureSize = 3;  // aperture parameter for Sobel operator (must be odd)
+    int minResponse = 100; // minimum value for a corner in the 8bit scaled response matrix
+    double k = 0.04;       // Harris parameter (see equation for details)
+
+    int nmsSize = 5; // non-maximum suppression size
+
+    // Apply corner detection
+    double t = (double)cv::getTickCount();
+    // Detect Harris corners and normalize output
+    cv::Mat dst, dst_norm, dst_norm_scaled, dst_thresh, dst_dilated, dst_maxima;
+    dst = cv::Mat::zeros(img.size(), CV_32FC1);
+    cv::cornerHarris(img, dst, blockSize, apertureSize, k, cv::BORDER_DEFAULT);
+    cv::normalize(dst, dst_norm, 0, 255, cv::NORM_MINMAX, CV_32FC1, cv::Mat());
+    cv::convertScaleAbs(dst_norm, dst_norm_scaled);
+
+    // dilate scaled image
+    dst_dilated = cv::Mat::zeros(img.size(), CV_8UC1);
+    cv::Mat dilationKernel = cv::getStructuringElement(cv::MORPH_ELLIPSE,
+                                                       cv::Size(2 * nmsSize + 1, 2 * nmsSize + 1),
+                                                       cv::Point(nmsSize, nmsSize));
+    cv::dilate(dst_norm_scaled, dst_dilated, dilationKernel);
+
+    // threshold scaled image, anything below minResponse is 0
+    dst_thresh = cv::Mat::zeros(img.size(), CV_8UC1);
+    cv::threshold(dst_norm_scaled, dst_thresh, minResponse, 255, cv::THRESH_TOZERO);
+
+    // maxima: compare threshold to dilated
+    dst_maxima = cv::Mat::zeros(img.size(), CV_8UC1);
+    cv::compare(dst_thresh, dst_dilated, dst_maxima, cv::CmpTypes::CMP_GE);
+
+    // keypoints from contour analysis
+    vector<vector<cv::Point>> contours;
+    cv::findContours(dst_maxima, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+
+    // analyze contours
+    for (const auto &contour : contours)
+    {
+        // centroid
+        cv::Point2f centroid;
+        cv::Moments moments = cv::moments(contour);
+        if (moments.m00 == 0)
+        {
+            // mean of contour points
+            for (const auto &point : contour)
+            {
+                centroid.x += point.x;
+                centroid.y += point.y;
+            }
+            centroid.x /= contour.size();
+            centroid.y /= contour.size();
+        }
+        else
+        {
+            centroid = cv::Point2f(
+                moments.m10 / moments.m00,
+                moments.m01 / moments.m00);
+        }
+
+        // value
+        int response = dst_thresh.at<uchar>(centroid.x, centroid.y);
+
+        cv::KeyPoint newKeyPoint;
+        newKeyPoint.pt = centroid;
+        newKeyPoint.size = nmsSize + 1;
+        newKeyPoint.response = response;
+
+        keypoints.push_back(newKeyPoint);
+    }
+
+    t = ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+    cout << "Harris detection with n=" << keypoints.size() << " keypoints in " << 1000 * t / 1.0 << " ms" << endl;
+
+    // visualize results
+    if (bVis)
+    {
+        cv::Mat visImage = img.clone();
+        cv::drawKeypoints(img, keypoints, visImage, cv::Scalar::all(-1), cv::DrawMatchesFlags::DRAW_RICH_KEYPOINTS);
+        string windowName = "Harris Corner Detector Results";
+        cv::namedWindow(windowName, 6);
+        imshow(windowName, visImage);
+        cv::waitKey(0);
+    }
+}
+
+//  FAST, BRISK, ORB, AKAZE, SIFT
+void detKeypointsModern(std::vector<cv::KeyPoint> &keypoints, cv::Mat &img, std::string detectorType, bool bVis)
+{
+
+    cv::Ptr<cv::FeatureDetector> detector;
+
+    if (detectorType.compare("FAST") == 0)
+    {
+        detector = cv::FastFeatureDetector::create();
+    }
+    else if (detectorType.compare("BRISK") == 0)
+    {
+        detector = cv::BRISK::create();
+    }
+    else if (detectorType.compare("ORB") == 0)
+    {
+        detector = cv::ORB::create();
+    }
+    else if (detectorType.compare("AKAZE") == 0)
+    {
+        detector = cv::AKAZE::create();
+    }
+    else if (detectorType.compare("SIFT") == 0)
+    {
+        detector = cv::xfeatures2d::SiftFeatureDetector::create();
+    }
+
+    // detect keypoints
+    detector->detect(img, keypoints);
+}
